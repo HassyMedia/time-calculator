3e735e06dfe3176ba5c7b6126fd55b1e8a1c4b4e
Ronnie Nehemia <33788692+HassyMedia@users.noreply.github.com>
2023-12-01 23:00:31 -0500

    Added example usage for testing.

--- time_calculator.py
+++ time_calculator.py
@@ -46,3 +46,9 @@ def add_time(start, duration, starting_day=None):
 
     return end_time
 
+print(add_time("3:00 PM", "3:10"))
+print(add_time("11:30 AM", "2:32", "Monday"))
+print(add_time("11:43 AM", "00:20"))
+print(add_time("10:10 PM", "3:30"))
+print(add_time("11:43 PM", "24:20", "tueSday"))
+print(add_time("6:30 PM", "205:12"))
